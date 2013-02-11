deaba7865848200a4d3084deeea2dc0c57047392
nathan <nathan@nathan-HP-ENVY-14-SPECTRE-Notebook-PC.(none)>
2013-02-11 20:29:06 +0000

    modified:   src/Game.java 	new file:   src/main.java

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,11 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  * The field for the game and shit.
@@ -8,9 +15,39 @@ public class Game extends JFrame {
 	
 	/**How fast the puck and shit moves.*/
 	private int level;
+	private Graphics2D draw;
+	/**Where shit in the window goes.*/
+	private Container pane;
 	
 	
-	public Game(){
+	public Game(String player1, String player2){
+		super("Pong");
+		initComps(player1, player2);
+		
+	}
+	
+	public void initComps(String player1, String player2){
+		
+		pane = getContentPane();
+		setSize(600,400);
+		Container names = new Container();
+		
+		pane.setLayout(new BorderLayout());
+		names.setLayout(new GridLayout(1,3));
+		
+		names.add(new JLabel(player1));
+		names.add(new JLabel(""));
+		names.add(new JLabel(player2));
+		
+		pane.add(names, BorderLayout.NORTH);
+		
+		setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.fillRect(30, 80, 500, 300);
 		
 	}
 	
---
+++ src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Game game = new Game("andy", "nathan");
+		
+
+	}
+
+}
