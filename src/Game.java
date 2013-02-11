298debdfdd35745456dca2a95388ceee6038bab0
nathan <nathan@nathan-HP-ENVY-14-SPECTRE-Notebook-PC.(none)>
2013-02-11 19:08:02 +0000

    modified:   src/Game.java 	modified:   src/Player.java

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,21 @@
+import javax.swing.JFrame;
 
-public class Game {
+/**
+ * The field for the game and shit.
+ *
+ */
+public class Game extends JFrame {
+	
+	/**How fast the puck and shit moves.*/
+	private int level;
+	
+	
+	public Game(){
+		
+	}
+	
+	public int getLevel(){
+		return level;
+	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,20 @@
 
 public class Player {
 
+	private int currentScore;
+	private String name;
+	
+	
+	public Player(String name){
+		this.name = name;
+		currentScore = 0;
+	}
+	
+	public int getScore(){
+		return currentScore;
+	}
+	
+	public void scored(){
+		currentScore++;
+	}
 }
